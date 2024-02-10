9d41ac714438e26d42f3277f9a958f25ba666149
WILLIAM POWELL <wgpowell@wisc.edu>
2024-02-10 16:07:09 -0600

    fixed bug. Added local and export builtins

--- project3/wsh.c
+++ project3/wsh.c
@@ -52,15 +52,69 @@ char *getVar(VAR *head, char* varName){
 	}
 }
 
+void editVar(VAR **vars, char *varName, char *varVal){
+	VAR *curr = *vars;
+	while (curr != NULL && strcmp(varName, curr->name) != 0){
+		if (curr->nextVar == NULL || curr->nextVar->isHead == 1){
+			//this function should only be called after checking of varName exists
+			//so if variable is not found its an error
+			printf("ERROR: Var not found");
+			exit(1);
+		} else {
+			curr = curr->nextVar;
+		}
 
-//TODO: FIX ME
-//everything is still inserted into the head
+	}
+
+	curr->contents = varVal;
+
+}
+
+void deleteVar(VAR **vars, char*varName){
+	VAR *curr = *vars;
+	printf("Deleting %s\n", varName);
+	if (curr == NULL){
+		return;
+	}
+	while (curr != NULL && strcmp(varName, curr->name) != 0){
+		if(curr->nextVar == NULL || curr->nextVar->isHead == 1){
+			return;
+		}
+		curr = curr->nextVar;
+	}
+
+	if (curr->nextVar == NULL){
+		//only one variable in the list
+		free(curr->contents);
+		free(curr->name);
+		free(curr);
+		*vars = NULL;
+	} else if (curr->nextVar == curr->prevVar){
+		// only two variables in the list
+		curr->nextVar->nextVar = NULL;
+		curr->nextVar->prevVar = NULL;
+		if (curr->isHead == 1){
+			curr->nextVar->isHead = 1;
+		}
+		free(curr->contents);
+		free(curr->name);
+		free(curr);
+	} else {
+		curr->nextVar->prevVar = curr->prevVar;
+		curr->prevVar->nextVar = curr->nextVar;
+		free(curr->contents);
+		free(curr->name);
+		free(curr);
+	}
+
+}
+
 void insertVar(VAR **vars, char* varName, char *varVal){
 
 	char *test = getVar(*vars, varName);
 
 	if (test != NULL){
-		printf("ERROR: Variable already assigned");
+		printf("ERROR: Variable already assigned\n");
 		return;
 	} 
 	
@@ -76,7 +130,7 @@ void insertVar(VAR **vars, char* varName, char *varVal){
 		head->nextVar = NULL;
 		head->prevVar = NULL;
 		head->isHead = 1;
-		
+
 	} else if ((*vars)->prevVar == NULL){
 		
 		VAR *head = *vars;
@@ -130,7 +184,7 @@ void changeDir(int argCount, char *args[]){
         }
 }
 
-void local(VAR *vars, int argCount, char *args[]){
+void local(VAR **vars, int argCount, char *args[]){
 	char delim = '=';
 	if (argCount != 2){
 		printf("USAGE: local VAR=<value>\n");
@@ -138,6 +192,11 @@ void local(VAR *vars, int argCount, char *args[]){
 	}
 	char *varName = args[1];
 	char *varVal = args[1];
+
+	if (*varName == '$'){
+		printf("Variable name cannot start with '$'");
+		return;
+	}
 
 	while(*varVal != '\0' && *varVal != delim){
 		varVal++;
@@ -151,10 +210,65 @@ void local(VAR *vars, int argCount, char *args[]){
 	*varVal = '\0';
 	varVal++;
 
-	insertVar(&vars, varName, varVal);
+	if (*varVal=='\0'){
+		deleteVar(vars, varName);
+	} else if(getVar(*vars, varName)!=NULL){
+		editVar(vars, varName, varVal);
+	} else {
+		insertVar(vars, varName, varVal);
+	}
+}
 
-	printf("varVal: %s\n", varVal);
-	printf("varName: %s\n", varName);
+void export(int argCount, char *args[]){
+	if (argCount != 2){
+		printf("USAGE: export VAR=<value>\n");
+		return;
+	}
+	
+	char delim = '=';
+	char *varName = args[1];
+	char *varVal = args[1];
+
+	if (*varName == '$'){
+		printf("Variable name cannot start with '$'");
+		return;
+	}
+
+	while(*varVal != '\0' && *varVal != delim){
+		varVal++;
+	}
+
+	if (*varVal == '\0'){
+		printf("USAGE: loval VAR=<value>\n");
+		return;
+	}
+
+	*varVal = '\0';
+	varVal++;
+
+	if(*varVal=='\0'){
+		if(unsetenv(varName) == -1){
+			printf("ERROR: unsetenv() failed\n");
+			exit(1);
+		}
+	} else {
+		if (setenv(varName, varVal, 0) == -1){
+			printf("ERROR: setenv() failed\n");
+			exit(1);
+		}
+	}
+}
+
+void printVars(VAR **vars){
+	VAR *curr = *vars;
+	while (curr != NULL){
+		printf("%s=%s\n", curr->name, curr->contents);
+		if (curr->nextVar == NULL || curr->nextVar->isHead == 1){
+			break;
+		} else {
+			curr = curr->nextVar;
+		}
+	}
 }
 
 void execCommand(char *args[]){
@@ -171,7 +285,7 @@ void execCommand(char *args[]){
 
 }
 
-void parseCommand(VAR* vars, int argCount, char*args[]){
+void parseCommand(VAR **vars, int argCount, char*args[]){
 
         if (strcmp(args[0], "exit") == 0){
         	exitShell(argCount);
@@ -179,6 +293,10 @@ void parseCommand(VAR* vars, int argCount, char*args[]){
         	changeDir(argCount, args);
         } else if (strcmp(args[0], "local") == 0){
         	local(vars, argCount, args);
+	} else if (strcmp(args[0], "vars") == 0){
+		printVars(vars);
+	} else if (strcmp(args[0], "export") == 0){
+		export(argCount, args);
         } else {
                 execCommand(args);
         }
@@ -227,7 +345,7 @@ void runInteractive(){
 		}
 		
         	free(buff);
-		parseCommand(vars, wc, args);
+		parseCommand(&vars, wc, args);
 		free(args);
 	}
 
